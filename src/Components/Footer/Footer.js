bf76ba95e6e7f4f552cf431fd6149c223a2fd857
Bhavesh588 <sbhavesh588@gmail.com>
2022-12-06 20:32:45 +0530

    Footer Component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -16,58 +16,66 @@ function Footer() {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg col_margin">
-                        <img src={require('../../assets/Felix Clavijo 1.png')} alt='footerLogo' style={{ borderRadius: 5 }} className="image_style" />
-                        <p className="my-4">Diseñamos personas inspiradas Experiencias que crean positivo cambio en la vida de las personas.</p>
-                        <div className="image_align">
-                            <img src={require('../../assets/QR-code-obituary.svg.webp')} alt='footerLogo' style={{ height: 90, borderRadius: 5 }} />
-                        </div>
-                    </div>
-                    <div className="col-lg col_margin">
-                        <span style={{ fontSize: 25, fontWeight: 600 }}>Useful Links</span>
-                        <ul>
-                            {
-                                navigation_data?.map((nav, i) => 
-                                    <li key={i}>
-                                        <Link to={nav.path} className={`nav-link text-theme ${pathname === nav.path ? 'active' : ''}`} onClick={() => setPathname(nav.path)}>
-                                            {nav.title}
-                                        </Link>
-                                    </li>
-                                )
-                            }
-                        </ul>
-                    </div>
-                    <div className="col-lg col_margin">
-                        <span style={{ fontSize: 25, fontWeight: 600 }}>Información</span>
-                        <div className="container-fluid">
-                            <div className="row my-3">
-                                <div className="col-2">
-                                    <FontAwesomeIcon icon="location-dot" className="text-theme" style={{ fontSize: 20 }} />
-                                </div>
-                                <div className="col-10">
-                                    <span>Nazca 366, Flores, CABA</span>
-                                </div>
-                            </div>
-                            <div className="row my-3">
-                                <div className="col-2">
-                                    <FontAwesomeIcon icon="envelope" className="text-theme" style={{ fontSize: 20 }} />
-                                </div>
-                                <div className="col-10">
-                                    <span>felixclavijo@gmail.com</span>
-                                </div>
+                        <div>
+                            <img src={require('../../assets/Felix Clavijo 1.png')} alt='footerLogo' style={{ borderRadius: 5 }} className="image_style" />
+                            <p className="my-4">Diseñamos personas inspiradas Experiencias que crean positivo cambio en la vida de las personas.</p>
+                            <div className="image_align">
+                                <img src={require('../../assets/QR-code-obituary.svg.webp')} alt='footerLogo' style={{ height: 90, borderRadius: 5 }} />
                             </div>
                         </div>
                     </div>
                     <div className="col-lg col_margin">
-                        <span style={{ fontSize: 25, fontWeight: 600 }}>Redes sociales</span>
-                        <div className="d-flex my-3">
-                            <div className="icon-theme mx-1">
-                                <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                        <div>
+                            <span style={{ fontSize: 25, fontWeight: 600 }}>Useful Links</span>
+                            <ul>
+                                {
+                                    navigation_data?.map((nav, i) => 
+                                        <li key={i}>
+                                            <Link to={nav.path} className={`nav-link text-theme ${pathname === nav.path ? 'active' : ''}`} onClick={() => setPathname(nav.path)}>
+                                                {nav.title}
+                                            </Link>
+                                        </li>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg col_margin">
+                        <div>
+                            <span style={{ fontSize: 25, fontWeight: 600 }}>Información</span>
+                            <div className="container-fluid">
+                                <div className="row my-3">
+                                    <div className="col-2">
+                                        <FontAwesomeIcon icon="location-dot" className="text-theme" style={{ fontSize: 20 }} />
+                                    </div>
+                                    <div className="col-10">
+                                        <span>Nazca 366, Flores, CABA</span>
+                                    </div>
+                                </div>
+                                <div className="row my-3">
+                                    <div className="col-2">
+                                        <FontAwesomeIcon icon="envelope" className="text-theme" style={{ fontSize: 20 }} />
+                                    </div>
+                                    <div className="col-10">
+                                        <span>felixclavijo@gmail.com</span>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="icon-theme mx-1">
-                                <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
-                            </div>
-                            <div className="icon-theme mx-1">
-                                <FontAwesomeIcon icon="fa-brands fa-whatsapp" />
+                        </div>
+                    </div>
+                    <div className="col-lg col_margin">
+                        <div>
+                            <span style={{ fontSize: 25, fontWeight: 600 }}>Redes sociales</span>
+                            <div className="d-flex my-3">
+                                <div className="icon-theme mx-1">
+                                    <FontAwesomeIcon icon="fa-brands fa-instagram" />
+                                </div>
+                                <div className="icon-theme mx-1">
+                                    <FontAwesomeIcon icon="fa-brands fa-linkedin-in" />
+                                </div>
+                                <div className="icon-theme mx-1">
+                                    <FontAwesomeIcon icon="fa-brands fa-whatsapp" />
+                                </div>
                             </div>
                         </div>
                     </div>
