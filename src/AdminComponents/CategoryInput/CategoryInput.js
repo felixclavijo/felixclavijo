e84efe7fe99a91fd0ea994eb720d46c873419c35
Bhavesh588 <sbhavesh588@gmail.com>
2023-01-04 09:10:08 +0530

    News Admin Page half finished

--- src/AdminComponents/CategoryInput/CategoryInput.js
+++ src/AdminComponents/CategoryInput/CategoryInput.js
@@ -13,13 +13,13 @@ function CategoryInput({ add=false, removeCat, ...props }) {
                     <div className="col-3 d-flex align-items-center justify-content-center title_box">{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</div>
                     <div className="col p-0 d-flex align-items-center justify-content-center input_box">
                         <input name={props.name} id={props.id} defaultValue={props.defaultValue} type={props.type} placeholder={props.placeholder} onChange={props.onChange} className="w-100 h-100" />
-                        <div className="icon_box"
-                            onClick={(e) => {
-                                removeCat(props.id)
-                            }}
-                        >
-                            <FontAwesomeIcon icon={add ? "plus" : "minus"} className="icon_style" />
-                        </div>
+                    </div>
+                    <div className="col-1 d-flex align-items-center justify-content-center icon_box2"
+                        onClick={(e) => {
+                            removeCat(props.id)
+                        }}
+                    >
+                        <FontAwesomeIcon icon={add ? "plus" : "minus"} className="icon_style" />
                     </div>
                 </div>
             </div>
